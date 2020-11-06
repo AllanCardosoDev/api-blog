116498dcca5cef932d6927a7036342fb11f1b453
jhonatan <jdsjhonatan@hotmail.com>
2020-11-06 11:02:37 -0300

    latestes update and adding db

--- app.py
+++ app.py
@@ -1,96 +1,218 @@
-# Explain that there are about 2 big players when it comes to creating elegant apis in python, one is using django, the other is using flask.
-# A Opção de usar o django é na minha visão muito mais complexa para quem está iniciado. Então escolhi ensinar flask por que, além de ser muito mais simples de enter, o flask por sí só é considerada uma micro-framework e por isso ele é extremamente leve, rápido e fácil de aplicar, sem muitas depêndencias.
-# Então agora entendendo que iremos usar o Flask para construir nosso api, temos 2 vamos chamar de modos diferentes de criar um api dentro do flask
-# As duas opções são FLASK comum e FLASK RESTFUL.
-# O flask restuful usa um biblioteca extra e na minha opinião e experiência adicona uma nivel de complexidade que pode dificultar muito para quem está iniciando e não consegui encontrar benefícios significativos que justifcasse ensinar usando ele.
-# Então iremos usar o flask comum que é infinitamente mais simples de criar e usar.
-# vamos lá
-# Para começar nós temos que importar algumas funcionalidades de dentro do módulo flask
-# Vamos precisa do flask em si
-# o método jsonify para processar os dados no formato json que serão usados em nossas apis
-# e o módulo request do flask(não cofunda ele com o módulo requets do python, esse request aqui(que inclusive está no singular) é exclusivo do flask
-# oo request aqui é útil porque é através dele que você conseguirá acessar os recursos que são passados para a API
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
+from flask_sqlalchemy import SQLAlchemy
+import jwt
+import datetime
+from functools import wraps
 
-# Para inicializar nossa aplicação web flask, temos que usar a seguinte sintaxe
 app = Flask(__name__)
-# Não se preocupe muito com isso, essa sintaxe está apenas dizendo que o arquivo atual deve executar e rodar um servidor flask
-# Então vamos ao passo a passo sobre como criar apis
-# Qual é
-'''
-1- Definir o objetivo da API:
-	ex: Iremos montar uma api de blog, onde eu poderei consultar, edit, criar e excluir postagens em um blog usando somente a api(maybe show them devaprender.com)
-2 - Qual será o URL base do api?
-	ex: Quando você cria uma aplicação local ela terá um url tipo http://localhost:5000 , porém quando você for subir isso para nuvem, você terá que comprar ou usar um domínio como url base, vamos imaginar um exemplo de devaprender.com/api/
-3 - Quais são os endpoints?
-	ex: Se seu requisito é de pode consultar, editar, criar e excluir, você terá que disponibilizar os endpoints para essas questões
-			> /postagens
-4 - Quais recursos será disponibilizado pelo api: informações sobre as postagens
-5 - Quais verbos http serão disponibilizados?
-	* GET
-	* POST
-	* PUT
-	* DELETE
-6 - Quais são os URL completos para cada um?
-	http://localhost:5000/postagens
-'''
-postagens = [
-    {
-        'título': 'Minha história',
-        'autor': 'Jhonatan de Souza'
-    },
-    {
-        'título': 'Como Instalar Python',
-        'autor': 'Jhonatan de Souza'
-    },
-    {
-        'título': 'Como Instalar Selenium',
-        'autor': 'Jhonatan de Souza'
-    }
-]
+
+# Configurações inicias
+app.config['SECRET_KEY'] = 'segredo2030'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+db: SQLAlchemy
 
 
-# Rota get padrão
-@app.route("/")
-def obter_todas_postagens():
-    return jsonify(postagens)
-
-# Explain how the routing works
-# Você deve especificar para como você espera receber os valores
-# E obrigatóriamente você precisa passar o mesmo parâmetro que é passado dentro de route,como parâmetro para a função abaixo ou não irá funcionar.
+class Postagem(db.Model):
+    __tablename__ = 'postagem'
+    id_postagem = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String)
+    id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor'))
 
 
-# Obter recurso com parâmetro
-@app.route("/postagens/<int:indice>", methods=['GET'])
-def obter_postagem(indice):
-    # request contem tudo que está vindo da requisição
-    return jsonify(postagens[indice], 200)
+class Autor(db.Model):
+    __tablename__ = 'autor'
+    id_autor = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String)
+    email = db.Column(db.String)
+    senha = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+    postagens = db.relationship('Postagem')
 
 
-# Criar novo recurso
-@app.route("/postagens", methods=['POST'])
-def nova_postagem():
+def token_obrigatorio(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # Verificar se foi incluído um token na requisição
+        if 'x-acess-token' in request.headers:
+            token = request.headers['x-acess-token']
+        # Se não houver um token, retornar a requesição, solicitando um token
+        if not token:
+            return jsonify({'mensagem': 'Token de autenticação precisa ser incluído nessa requisição!'})
+        try:
+            dados = jwt.decode(token, app.config['SECRET_KEY'])
+            autor_atual = Autor.query.filter_by(
+                id_autor=dados['id_autor']).first()
+        except:
+            return jsonify({'mensagem': 'Token é inválido!'}, 401)
+
+        return f(autor_atual, *args, **kwargs)
+    # retornando o decorador
+    return decorated
+
+
+@app.route('/postagens', methods=['GET'])
+@token_obrigatorio
+def obter_todas_postagens(autor_atual):
+    postagens = Postagem.query.all()
+
+    lista_postagens = []
+    for postagem in postagens:
+        dados_postagem = {}
+        dados_postagem['titulo'] = postagem.titulo
+        dados_postagem['id_autor'] = postagem.id_autor
+        lista_postagens.append(dados_postagem)
+
+    return jsonify({'postagens': lista_postagens})
+
+
+@app.route('/postagens/<int:id_postagem>', methods=['GET'])
+@token_obrigatorio
+def obter_postagem_por_id(autor_atual, id_postagem):
+    postagem = Postagem.query.filter_by(id_postagem=id_postagem).first()
+
+    dados_postagem = {}
+    dados_postagem['titulo'] = postagem.titulo
+    dados_postagem['id_autor'] = postagem.id_autor
+
+    return jsonify({'postagens': dados_postagem})
+
+
+@app.route('/postagens', methods=['POST'])
+@token_obrigatorio
+def nova_postagem(autor_atual):
     postagem = request.get_json()
-    postagens.append(postagem)
-    return jsonify({'id': len(postagens)}, 200)
+
+    nova_postagem = Postagem(
+        titulo=postagem['titulo'], id_autor=postagem['id_autor'])
+
+    db.session.add(nova_postagem)
+    db.session.commit()
+
+    return jsonify({'nova postagem': 'postagem criado com sucesso!'})
 
 
-# Editar recurso existente
-@app.route("/postagens/<int:indice>", methods=['PUT'])
-def atualizar_postagem(indice):
-    test = request.get_json()
-    postagens[indice].update(test)
-    return jsonify(postagens[indice], 200)
+@app.route('/postagens/<int:id_postagem>', methods=['PUT'])
+@token_obrigatorio
+def atualizar_postagem(autor_atual, id_postagem):
+    dados = request.get_json()
+    postagem = Postagem.query.filter_by(id_postagem=id_postagem).first()
+
+    postagem.titulo = dados['titulo']
+    postagem.id_autor = dados['id_autor']
+
+    db.session.commit()
+
+    return jsonify({'mensagem': 'Postagem alterada com sucesso!'})
 
 
-# Excluir um recurso
-@app.route('/postagens/<int:indice>', methods=['DELETE'])
-def excluir_postagem(indice):
-    postagem = postagens[indice]
-    del postagens[indice]
-    return jsonify(f'Foi excluído a postagem {postagem}', 200)
+@app.route('/postagens/<int:id_postagem>', methods=['DELETE'])
+@token_obrigatorio
+def excluir_postagem(autor_atual, id_postagem):
+    postagem = Postagem.query.filter_by(id_postagem=id_postagem).first()
+
+    if not postagem:
+        return jsonify({'mensagem': 'Postagem não encontrada'})
+
+    db.session.delete(postagem)
+    db.session.commit()
+
+    return jsonify({'mensagem': 'A postagem foi excluída!'})
+
+# * Criar uma api para criar novos autores
 
 
-# test
-if __name__ == "__main__":
+@app.route('/autores', methods=['GET'])
+@token_obrigatorio
+def obter_todos_autores(autor_atual):
+    autores = Autor.query.all()
+    lista_de_autores = []
+    for autor in autores:
+        dados_autores = {}
+        dados_autores['id_autor'] = autor.id_autor
+        dados_autores['nome'] = autor.nome
+        dados_autores['email'] = autor.email
+        lista_de_autores.append(dados_autores)
+
+    return jsonify({'autores': lista_de_autores})
+
+
+@app.route('/autores/<int:id_autor>', methods=['GET'])
+@token_obrigatorio
+def obter_autor_por_id(autor_atual, id_autor):
+    autor = Autor.query.filter_by(id_autor=id_autor).first()
+
+    if not autor:
+        return jsonify({'mensagem': 'Autor não encontrado'})
+
+    dados_autor = {}
+    dados_autor['id_autor'] = autor.id_autor
+    dados_autor['nome'] = autor.nome
+    dados_autor['email'] = autor.email
+
+    return jsonify({'autor': dados_autor})
+
+
+@app.route('/autores', methods=['POST'])
+@token_obrigatorio
+def novo_autor(autor_atual):
+    dados = request.get_json()
+
+    novo_usuario = Autor(nome=dados['nome'],
+                         senha=dados['senha'], email=dados['email'])
+
+    db.session.add(novo_usuario)
+    db.session.commit()
+
+    return jsonify({'mensagem': 'Novo usuário criado com sucesso!'}, 200)
+
+
+@app.route('/autores/<int:id_autor>', methods=['PUT'])
+@token_obrigatorio
+def atualizar_ator(autor_atual, id_autor):
+    autor = Autor.query.filter_by(id_autor=id_autor).first()
+    dados_autor = request.get_json()
+
+    autor.nome = dados_autor['nome']
+    autor.email = dados_autor['email']
+
+    db.session.commit()
+
+    return jsonify({'mensagem': 'autor foi alterado com sucesso!'})
+
+
+@app.route('/autores/<int:id_autor>', methods=['DELETE'])
+@token_obrigatorio
+def excluir_autor(autor_atual, id_autor):
+    autor = Autor.query.filter_by(id_autor=id_autor).first()
+
+    if not autor:
+        return jsonify({'mensagem': 'Autor não encontrado'})
+
+    db.session.delete(autor)
+    db.session.commit()
+
+    return jsonify({'mensagem': ' Autor excluído com sucesso!'})
+
+
+@app.route('/login')
+def login():
+    dados_autenticacao = request.authorization
+
+    if not dados_autenticacao or not dados_autenticacao.username or not dados_autenticacao.password:
+        return make_response('Login inválido', 401, {'WWW-Authenticate': 'Basic realm="Login obrigatório!"'})
+
+    user = Autor.query.filter_by(nome=dados_autenticacao.username).first()
+    if user.senha == dados_autenticacao.password:
+        token = jwt.encode({'id_autor': user.id_autor, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Login inválido', 401, {'WWW-Authenticate': 'Basic realm="Login obrigatório!"'})
+
+
+if __name__ == '__main__':
     app.run(port=5000, host='localhost', debug=True)
